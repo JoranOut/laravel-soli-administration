9609ec01accb36b0b8914e016a4c75d51741a139
JoranOut <jorankorverout@digiboox.app>
2026-03-21 12:21:44 +0100

    Register Passport scopes via tokensCan to fix invalid_scope error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,6 +33,13 @@ class AppServiceProvider extends ServiceProvider
 
     protected function configurePassport(): void
     {
+        Passport::tokensCan([
+            'openid' => 'Enable OpenID Connect',
+            'profile' => 'Access user profile (name)',
+            'email' => 'Access user email address',
+            'roles' => 'Access user roles',
+        ]);
+
         Passport::defaultScopes(['openid']);
 
         Passport::authorizationView(fn (array $params) => inertia('auth/oauth-authorize', $params));
